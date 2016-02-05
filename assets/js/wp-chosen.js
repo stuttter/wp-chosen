5cc5e270f90bda2b0f9111cfab3f7f1d07da0083
John James Jacoby <johnjamesjacoby@me.com>
2016-02-05 11:52:05 -0600

    Bump to 13, which is a cheat to help with Event Calendar hour & minute.

--- assets/js/wp-chosen.js
+++ assets/js/wp-chosen.js
@@ -2,13 +2,13 @@ jQuery( document ).ready( function ( $ ) {
 
 	/* Options */
 	var chosen_options = {
-		disable_search_threshold: 10
+		disable_search_threshold: 13
 	};
 
 	/* Targets */
-	$( '.wrap .actions:not(.bulkactions) select' ).chosen( chosen_options );
-	$( '.wrap .form-table select' ).chosen( chosen_options );
-	$( '#posts-filter .filter-items select' ).chosen( chosen_options );
 	$( '.media-toolbar select' ).chosen( chosen_options );
+	$( '.wrap .form-table select' ).chosen( chosen_options );
+	$( '.wrap .actions:not(.bulkactions) select' ).chosen( chosen_options );
 	$( '#customize-theme-controls select' ).chosen( chosen_options );
+	$( '#posts-filter .filter-items select' ).chosen( chosen_options );
 } );
--- wp-chosen.php
+++ wp-chosen.php
@@ -50,5 +50,5 @@ function wp_chosen_get_plugin_url() {
  * @return int
  */
 function wp_chosen_get_asset_version() {
-	return 201602050002;
+	return 201602050003;
 }
