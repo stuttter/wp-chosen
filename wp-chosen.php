cd4bbc727bfc6254ae5c7f66743b0c360d84cb40
John James Jacoby <johnjamesjacoby@me.com>
2016-04-01 17:56:56 -0500

    Use Chosen on Bulk Actions, now that "Search" is limited.
    Bump to 0.5.0.

--- wp-chosen.php
+++ wp-chosen.php
@@ -5,7 +5,7 @@
  * Plugin URI:  http://wordpress.org/plugins/wp-chosen/
  * Author:      John James Jacoby
  * Author URI:  http://jjj.me
- * Version:     0.4.0
+ * Version:     0.5.0
  * Description: Makes long, unwieldy select boxes much more user-friendly.
  * License:     GPLv2 or later
  */
@@ -45,5 +45,5 @@ function wp_chosen_get_plugin_url() {
  * @return int
  */
 function wp_chosen_get_asset_version() {
-	return 201602230004;
+	return 201604010001;
 }
