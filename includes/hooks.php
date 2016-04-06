a3ad731491a3b6e6622762e037b9b22304705121
John James Jacoby <johnjamesjacoby@me.com>
2016-04-06 13:07:47 -0500

    Try enqueueing chosen in Customizer; see what explodes.

--- includes/hooks.php
+++ includes/hooks.php
@@ -10,7 +10,8 @@
 defined( 'ABSPATH' ) || exit;
 
 // Enqueue admin assets
-add_action( 'admin_enqueue_scripts', 'wp_chosen_enqueue_assets', -99 );
+add_action( 'admin_enqueue_scripts',              'wp_chosen_enqueue_assets', -99 );
+add_action( 'customize_controls_enqueue_scripts', 'wp_chosen_enqueue_assets', -99 );
 
 // options-reading.php juggle
 add_action( 'admin_head',               'wp_options_reading_juggle', -99 );
